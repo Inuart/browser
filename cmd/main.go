56a5b3e78185346eddcfbe3c4cff59be5ef5ca31
Eduard Castany <eduard.castany@gmail.com>
2021-06-11 16:18:21 +0200

    simplify example cmd

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,7 @@ package main
 import "github.com/Inuart/browser"
 
 func main() {
-	cfg := browser.Config{
-		Private: true,
-		AsApp:   true,
-	}
-	if err := cfg.Chrome("http://google.com"); err != nil {
+	if err := browser.Preferred("https://google.com"); err != nil {
 		println(err.Error())
 	}
 }
